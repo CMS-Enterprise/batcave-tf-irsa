8240a6963f30e5f5fd15fdce464d71b4319caa5a
Arun <arunchowdary.sanna@cms.hhs.gov>
2022-08-31 06:45:36 -0700

    code refactor

--- main.tf
+++ main.tf
@@ -1,54 +1,10 @@
-data "aws_iam_policy_document" "this" {
-  count = var.create_role ? 1 : 0
-
-  dynamic "statement" {
-    for_each = var.oidc_providers
-
-    content {
-      effect  = "Allow"
-      actions = ["sts:AssumeRoleWithWebIdentity"]
-
-      principals {
-        type        = "Federated"
-        identifiers = [statement.value.provider_arn]
-      }
-
-      condition {
-        test     = var.assume_role_condition_test
-        variable = "${replace(statement.value.provider_arn, "/^(.*provider/)/", "")}:sub"
-        values   = [for sa in statement.value.namespace_service_accounts : "system:serviceaccount:${sa}"]
-      }
-
-      # https://aws.amazon.com/premiumsupport/knowledge-center/eks-troubleshoot-oidc-and-irsa/?nc1=h_ls
-      condition {
-        test     = var.assume_role_condition_test
-        variable = "${replace(statement.value.provider_arn, "/^(.*provider/)/", "")}:aud"
-        values   = ["sts.amazonaws.com"]
-      }
-
-    }
-  }
-}
-
-resource "aws_iam_role" "this" {
-  count = var.create_role ? 1 : 0
-
-  name        = var.role_name
-  name_prefix = var.role_name_prefix
-  path        = var.role_path
-  description = var.role_description
-
-  assume_role_policy    = data.aws_iam_policy_document.this[0].json
-  max_session_duration  = var.max_session_duration
-  permissions_boundary  = var.role_permissions_boundary_arn
-  force_detach_policies = var.force_detach_policies
-
-  tags = var.tags
-}
-
-resource "aws_iam_role_policy_attachment" "this" {
-  for_each = { for k, v in var.role_policy_arns : k => v if var.create_role }
-
-  role       = aws_iam_role.this[0].name
-  policy_arn = each.value
+module "iam_assumable_role_admin" {
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  create_role                   = true
+  role_name                     = "${var.cluster_name}-cluster-autoscaler"
+  provider_url                  = replace(var.cluster_oidc_issuer_url, "https://", "")
+  role_policy_arns              = [aws_iam_policy.batcave_autoscaler.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:${local.k8s_service_account_namespace}:${local.k8s_service_account_name}"]
+  role_path                     = var.iam_path
+  role_permissions_boundary_arn = var.permissions_boundary
 }
